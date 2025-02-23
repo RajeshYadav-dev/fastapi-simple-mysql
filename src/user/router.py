b191d041d61e5b15f3cdcde1b7d1d327705a2f4e
Rajesh Yadav <rajeshyadav0565@gmail.com>
2025-02-23 13:59:51 +0530

    added services

--- src/user/router.py
+++ src/user/router.py
@@ -1,8 +1,5 @@
 from fastapi import status, HTTPException,APIRouter
-import hashlib
-import mysql.connector
 from user.schemas import UserModel,LoginUserModel
-from database import dbcon
 from user.user_service import UserService
 
 user_router = APIRouter()
@@ -11,79 +8,45 @@ service = UserService()
 # Get all users
 @user_router.get("/users", status_code=status.HTTP_200_OK)
 def get_users():
-    mydb = dbcon.get_db_connection()
-    cursor = mydb.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM user;")
-    users = cursor.fetchall()
-    cursor.close()
-    mydb.close()
-    return users
+    return service.get_users()
 
 # Get a single user
 @user_router.get("/users/{user_id}", status_code=status.HTTP_200_OK)
 def get_a_user(user_id: int):
-    mydb = dbcon.get_db_connection()
-    cursor = mydb.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM user WHERE id = %s;", (user_id,))
-    user = cursor.fetchone()
-    cursor.close()
-    mydb.close()
+    user=service.get_a_user(user_id)
     if user:
         return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
 
 # Create a user
 @user_router.post("/", status_code=status.HTTP_201_CREATED)
-def create_a_user(user: UserModel):
-    mydb = dbcon.get_db_connection()
-    cursor = mydb.cursor()
-    hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
-    try:
-        cursor.execute(
-            "INSERT INTO user (username, password, email) VALUES (%s, %s, %s);",
-            (user.username, hashed_password, user.email)
-        )
-        mydb.commit()
-        user_id = cursor.lastrowid
-    except mysql.connector.Error as err:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Error: {err}")
-    finally:
-        cursor.close()
-        mydb.close()
-    return {"message": "User added successfully.", "user_id": user_id}
+def create_a_user(user_data: UserModel):
+    user = service.create_a_user(user_data)
+    if user:
+        print(user)
+        return user
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User details is not valid.")
 
 # Update a user
 @user_router.put("/{user_id}", status_code=status.HTTP_200_OK)
-def update_a_user(user_id: int, user: UserModel):
-    mydb = dbcon.get_db_connection()
-    cursor = mydb.cursor()
-    hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
-    cursor.execute(
-        "UPDATE user SET username = %s, password = %s, email = %s WHERE id = %s;",
-        (user.username, hashed_password, user.email, user_id)
-    )
-    mydb.commit()
-    if cursor.rowcount == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
-    cursor.close()
-    mydb.close()
-    return {"message": "User updated successfully."}
+def update_a_user(user_id: int, user_data: UserModel):
+    user=service.update_a_user(user_id,user_data)
+    if user:
+       return user
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User detail is invalid")
 
 # Delete a user
 @user_router.delete("/{user_id}", status_code=status.HTTP_200_OK)
 def delete_a_user(user_id: int):
-    mydb = dbcon.get_db_connection()
-    cursor = mydb.cursor()
-    cursor.execute("DELETE FROM user WHERE id = %s;", (user_id,))
-    mydb.commit()
-    if cursor.rowcount == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
-    cursor.close()
-    mydb.close()
-    return {"message": "User deleted successfully."}
+   data=service.delete_a_user(user_id)
+   if data:
+       return data
+   else:
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="user not deleted.") 
 
-
-# Create a user
+# login a user
 @user_router.post("/login", status_code=status.HTTP_200_OK)
 def login_a_user(user: LoginUserModel):
     dbuser=service.login_a_user(user)
--- src/user/user_service.py
+++ src/user/user_service.py
@@ -5,6 +5,86 @@ from database import dbcon
 
 
 class UserService():    
+  
+  def get_users(self):
+    mydb = dbcon.get_db_connection()
+    cursor = mydb.cursor(dictionary=True)
+    try:
+      cursor.execute("SELECT * FROM user;")
+      users = cursor.fetchall()
+      if len(users)>0:
+        return users
+    except mysql.connector.Error as err:
+        raise Exception(f"Something went wrong:{err}")
+    finally:
+      cursor.close()
+      mydb.close()
+      
+  
+  def get_a_user(self,user_id: int):
+    mydb = dbcon.get_db_connection()
+    cursor = mydb.cursor(dictionary=True)
+    try:
+      cursor.execute("SELECT * FROM user WHERE id = %s;", (user_id,))
+      user = cursor.fetchone()
+      if user:
+        return user
+    except mysql.connector.Error as err:
+        raise Exception(f"Something went wrong:{err}")
+    finally:
+      cursor.close()
+      mydb.close()
+      
+  def create_a_user(self,user_data: UserModel):
+    mydb = dbcon.get_db_connection()
+    cursor = mydb.cursor()
+    hashed_password = hashlib.sha256(user_data.password.encode()).hexdigest()
+    try:
+        cursor.execute(
+            "INSERT INTO user (username, password, email) VALUES (%s, %s, %s);",
+            (user_data.username, hashed_password, user_data.email)
+        )
+        mydb.commit()
+        return user_data
+    except mysql.connector.Error as err:
+        raise Exception(f"Something went wrong:{err}")
+    finally:
+        cursor.close()
+        mydb.close()      
+    
+  def update_a_user(self,user_id: int, user: UserModel):
+    mydb = dbcon.get_db_connection()
+    cursor = mydb.cursor()
+    try:
+      hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
+      cursor.execute(
+          "UPDATE user SET username = %s, password = %s, email = %s WHERE id = %s;",
+          (user.username, hashed_password, user.email, user_id)
+      )
+      mydb.commit()
+      if cursor.rowcount == 0:
+          return {"message:":"user updated"}
+    except mysql.connector.Error as err:
+        raise Exception(f"Something went wrong:{err}")
+    finally:
+      cursor.close()
+      mydb.close()    
+  
+  
+  def delete_a_user(self,user_id: int):
+    mydb = dbcon.get_db_connection()
+    cursor = mydb.cursor()
+    try:
+      cursor.execute("DELETE FROM user WHERE id = %s;", (user_id,))
+      mydb.commit()
+      if cursor.rowcount > 0:
+        return {"message:":"user deleted"}
+    except mysql.connector.Error as err:
+        raise Exception(f"Something went wrong:{err}") 
+    finally:
+      cursor.close()
+      mydb.close()
+    
   def login_a_user(self,user: LoginUserModel):
     mydb = dbcon.get_db_connection()
     cursor = mydb.cursor()
